0973c496cad13101af2c74500e7ffaadb717696f
callumhemming <callum.m.hemming@outlook.com>
2022-02-09 14:07:51 +0000

    BNranchcccc

--- tic-tac-toe/src/components/Gameboard/index.js
+++ tic-tac-toe/src/components/Gameboard/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import Row from "../Row";
 
 
-export default function Gameboard({ gameState }){
+export default function Gameboard({setGameState , gameState }){
 
     return(
  <>
 
-        <Row row={0} gameState={gameState}  />
-        <Row row={1} gameState={gameState}  />
-        <Row row={2} gameState={gameState}  />
+        <Row setGameState={setGameState} row={0} gameState={gameState}  />
+        <Row setGameState={setGameState} row={1} gameState={gameState}  />
+        <Row setGameState={setGameState} row={2} gameState={gameState}  />
  </>
 
     )
--- tic-tac-toe/src/components/Row/index.js
+++ tic-tac-toe/src/components/Row/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import css from "./Row.module.css"
 
 
-export default function Row({gameState , row}){
+export default function Row({gameState , row, setGameState}){
     const myRow = gameState[row]
 
+    function handleClick(cell){
+        const newArray = [...myRow][cell] = "newValue"
+        setGameState( ...gameState.slice(0,row),  newArray, ...gameState.slice(row)  )
+    }
 
 
     return(
     <div className={css.Row}>
         <button className = {css.Cell}>{myRow[0]}</button>
         <button className = {css.Cell}>{myRow[1]}</button>
-        <button className = {css.Cell}>{myRow[2]}</button>
+        <button onClick={()=>handleClick(2)} className = {css.Cell}>{myRow[2]}</button>
     </div>
     )
 }
