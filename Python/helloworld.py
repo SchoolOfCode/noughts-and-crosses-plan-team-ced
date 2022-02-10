1a5fc38badba4651f2875e1676f46052cf367e5d
callumhemming <82006291+callumhemming@users.noreply.github.com>
2022-02-10 15:45:21 +0000

    This is now a python repo

---
+++ Python/helloworld.py
@@ -0,0 +1,96 @@
+# Billie Eilish
+import webbrowser
+import os
+
+
+print("Hello! Would you like to play a game of trivai?")
+print("- Yes")
+print("- No")
+
+res = input().lower()
+
+if(res == "no"):
+    print("die")
+    exit()
+elif(res == "yes"):
+    print("GOOD LET'S GOOOOOOOOOOOO")
+else:
+    print("i asked you ONE question with TWO options you fucking idiot, learn to spell, fuck off and leave me alone")
+    webbrowser.open("https://www.youtube.com/watch?v=PV3_UHG73oQ", new=1)
+    exit()
+
+
+os.system('cls||clear')
+
+print("Which flies a green, white, and orange (in that order) tricolour flag?")
+
+print("Ireland")
+print("Ivory Coast")
+print("Italy")
+
+res = input().lower()
+print(res)
+if(res == "italy"):
+    print("OOo we got a little nerd on our hands over here")
+else:
+    print("'You're a failure'- your mother... probably")
+    exit()
+
+
+
+
+
+# 2) Which flies a green, white, and orange (in that order) tricolour flag? 
+
+# Ireland
+# Ivory Coast
+# Italy
+# Italy
+
+# 3) What company makes the Xperia model of smartphone?
+
+# Samsung
+# Sony 
+# Nokia
+# Sony
+
+# 4) Which city is home to the Brandenburg Gate?
+
+# Vienna
+# Zurich
+# Berlin
+# Berlin
+
+# 5) Which of the following is NOT a fruit?
+
+# Rhubarb
+# Tomatoes
+# Avocados
+# Rhubarb
+
+# 6) Where was the first example of paper money used?
+
+# China
+# Turkey
+# Greece
+# China
+
+# 7) Who is generally considered the inventor of the motor car?
+
+# Henry Ford
+# Karl Benz
+# Henry M. Leland
+# Karl Benz
+
+# 8) If you were looking at Iguazu Falls, on what continent would you be?
+
+# Asia
+# Africa
+# South America
+# South America
+
+# 9) What number was the Apollo mission that successfully put a man on the moon for the first time in human history? 
+
+# Apollo 11
+# Apollo 12
+# Apollo 13
